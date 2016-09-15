80615a3dea30644314cca03f1b836d8623fce08f
Anton Zaroutski <anton@zaroutski.com>
2016-09-15 23:34:27 +1000

    Removing custom content elements

---
+++ ia/ia.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Information infrastructure definition
+ * Description: This plugin defines all information infrastructure used for this project
+ * Version: 1.0.0
+ * Author: Anton Zaroutski
+ * Author URI: http://zarotuski.com
+ * License: GPL2
+ */
